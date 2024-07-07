11e4b8c06d0c3154e24244adf3cb7b8494bd7008
sunnyp06 <sunnyp06@gmail.com>
2024-07-07 17:13:30 +0000

    add more to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,9 +3,12 @@ import { Stack, Typography } from "@mui/material";
 export default function Home() {
   return (
     <main>
-      <Stack>
+      <Stack spacing={2}>
         <Typography variant="h1" sx={{ textAlign: "center" }}>
           Hi. I&apos;m Sunny.
+        </Typography>
+        <Typography variant="h5" sx={{ textAlign: "center" }}>
+          Product-focused Full Stack Developer
         </Typography>
       </Stack>
     </main>
