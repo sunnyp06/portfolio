ca1eb8637f859a400dee923fe8433fd48a31d71c
Sunny Ughareja <sunnyp06@gmail.com>
2024-07-07 13:00:58 -0400

    start home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+import { Stack, Typography } from "@mui/material";
+
 export default function Home() {
   return (
     <main>
-      <div>Hi</div>
+      <Stack>
+        <Typography variant="h1" sx={{ textAlign: "center" }}>
+          Hi. I&apos;m Sunny.
+        </Typography>
+      </Stack>
     </main>
   );
 }
