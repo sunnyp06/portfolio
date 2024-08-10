005d0527a45341cc5a32ba99fbd851e28aab16d9
Sunny Ughareja <sunnyp06@gmail.com>
2024-08-10 14:04:58 -0400

    about me content

--- src/app/about-me/page.tsx
+++ src/app/about-me/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Divider, Typography } from "@mui/material";
+import { Container, Divider, Stack, Typography } from "@mui/material";
 import Grid2 from "@mui/material/Unstable_Grid2";
 import Image from "next/image";
 
@@ -21,6 +21,7 @@ export default function AboutMe() {
               src="/profile-photo.png"
               width={1398}
               height={1398}
+              // TODO investigate performance impact
               // sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw"
               alt="profile photo"
               style={{
@@ -29,29 +30,25 @@ export default function AboutMe() {
                 borderRadius: "999px",
               }}
             />
-            {/* <Avatar
-              src="/profile-photo.png"
-              sx={{ aspectRatio: "1", minWidth: "50%", minHeight: "50%" }}
-            ></Avatar> */}
           </Grid2>
-          <Grid2
-            xs={12}
-            sm={6}
-            display="flex"
-            alignItems="center"
-            justifyContent="center"
-          >
-            <Typography variant="body2" align="left">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the standard dummy text ever since
-              the 1500s, when an unknown printer took a galley of type and
-              scrambled it to make a type specimen book. It has survived not
-              only five centuries, but also the leap into electronic
-              typesetting, remaining essentially unchanged. It was popularised
-              in the 1960s with the release of Letraset sheets containing Lorem
-              Ipsum passages, and more recently with desktop publishing software
-              like Aldus PageMaker including versions of Lorem Ipsum.
-            </Typography>
+          <Grid2 xs={12} sm={6} display="flex" alignItems="center">
+            <Stack spacing={2}>
+              <Typography variant="h3">
+                I&apos;m a software engineer based in NYC.
+              </Typography>
+              <Typography variant="body1">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the standard dummy text ever
+                since the 1500s, when an unknown printer took a galley of type
+                and scrambled it to make a type specimen book. It has survived
+                not only five centuries, but also the leap into electronic
+                typesetting, remaining essentially unchanged.
+              </Typography>
+              <Typography variant="body1">
+                Outside work, you can find me running, climbing, or listening to
+                or making music.
+              </Typography>
+            </Stack>
           </Grid2>
         </Grid2>
       </Container>
